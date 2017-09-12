1ae561e91c502813b8f0cd690f5629c8140077e0
Michał <michal.bootcamp@gmail.com>
2017-09-12 09:17:01 +0200

    ujednolicenie wciec

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var span = $("span");
 span.each(function(index, element) {
-    if(index % 2 == 0) {
-        $(element).css('color', 'red');
+	if(index % 2 == 0) {
+		$(element).css('color', 'red');
 	}
 });
 var paragraphs = $('p');
