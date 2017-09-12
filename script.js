0e1861037aba803d8db22ec2e2798a34b2b0dc06
Michał <michal.bootcamp@gmail.com>
2017-09-12 09:13:57 +0200

    poprawa wciec

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 var span = $("span");
- span.each(function(index, element) {
-	if(index % 2 == 0) {
-	$(element).css('color', 'red');
+span.each(function(index, element) {
+    if(index % 2 == 0) {
+        $(element).css('color', 'red');
 	}
 });
 var paragraphs = $('p');
 
 paragraphs.each(function(index, element) {
-	
+
 	var button = '<button class="btn" data-tmp="' + index + '">Click me</button>';
 
 	$(element).append(button);
@@ -15,6 +15,6 @@ paragraphs.each(function(index, element) {
 });
 
 
-$("button").click(function(){
+$("button").click(function() {
 	alert($(this).attr("data-tmp"));
 });
